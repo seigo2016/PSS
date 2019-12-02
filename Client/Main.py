a5668287c2cac4f79d7db94d03f378accfd66182
seigo2016 <2003seigo@gmail.com>
2019-12-02 21:11:27 +0900

    recreate

--- Client/Main.py
+++ Client/Main.py
@@ -1,84 +1,93 @@
 # -*- coding: utf-8 -*-
 import socket
-from multiprocessing import Array Pool, Process
 from threading import Thread
-import multiprocessing
-import cv2
 import random
-import numpy as np
-from PIL import ImageFont, ImageDraw, Image
-# from collections import deque
+import tkinter.font as font
+import tkinter as tk
+import time
+import os
+from PIL import Image, ImageTk, ImageFile
+import glob
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+root = tk.Tk()
+root.state('zoomed')
+# root.geometry("3840x2160")
+# root.wm_attributes('-fullscreen', 1)
+# root.tk.call("::tk::unsupported::MacWindowStyle",
+#              "style", root._w, "plain", "none")
+root.wm_attributes("-topmost", True)
+w = root.winfo_screenwidth()
+h = root.winfo_screenheight()
+img = []
+for i in range(1, 20):
+    tmp = Image.open(r"Tmp/page{}.png".format(i))
+    tmp = tmp.resize((w, h))
 
-# q = deque([[]], 100)
-q = [[None, 0, 0] for i in range(50)]
+    img.append(ImageTk.PhotoImage(tmp))
 
-width = 1600
-height = 900
+canvas = tk.Canvas(root, width=w, height=h)
+labelimg = canvas.create_image(w / 2, h / 2, image=img[0])
+canvas.pack()
+font = tk.font.Font(root, family="System", size=80)
+page = 1
+pagemax = len(glob.glob("Tmp/*"))
+
+
+class move_text:
+    def __init__(self, canvas, comment):
+        self.canvas = canvas
+        self.text = self.canvas.create_text(
+            w - 30, random.uniform(
+                2.0, 8.0) * 100, text=comment, font=font)
+        root.after(10, self.update)
+        # root.update()
+
+    def update(self):
+        self.canvas.move(self.text, -1, 0)
+        root.after(5, self.update)
+
+
+def next(event):
+    global page
+    if pagemax - 1 > page:
+        canvas.itemconfig(labelimg, image=img[page])
+        page += 1
+
+
+def prev(event):
+    global page
+    if 0 < page:
+        canvas.itemconfig(labelimg, image=img[page])
+        page -= 1
 
 
 def rcv_comment():
-    global q
+    global canvas
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('45.76.215.122', 10023))
-    s.sendall(b'seigo:password')
+    idpass = os.environ.get('PSSID')
+    print(idpass)
+    s.sendall(bytes(idpass))
     while True:
         try:
             data = s.recv(1024)
             if len(data) != 0:
                 comment = data.decode('utf-8')
                 print("recv:" + comment)
-                # print(type(comment))
                 if comment == "認証エラー":
                     break
-                else:
-                    for i in range(len(q)):
-                        if q[i][0] is None:
-                            q[i] = [comment, 0, random.uniform(50, height)]
-                            break
-                        print(q)
+                move_text(canvas, comment)
+            time.sleep(3)
         except KeyboardInterrupt:
             break
             s.close
         s.close
-        # except Exception as e:
-        #     print(e)
-        #     print("エラー")
     s.close
 
 
-def slide(list_q):
-    font = ImageFont.truetype("./ipaexg.ttf", 64)
-    page = 0
-    img = cv2.imread('./Temp/test{}.png'.format(page))
-    img = cv2.resize(img, dsize=(1920, 1080))
-    for i in range(len(list_q)):
-        if list_q[i][1] > 1800:
-            list_q[i] = [None, 0, 0]
-        elif list_q[i][0] is not None:
-            print("comment:" + list_q[i][0])
-            img_pil = Image.fromarray(img)
-            draw = ImageDraw.Draw(img_pil)
-            draw.text((1600 - int(list_q[i][1]), int(list_q[i][2])),
-                      list_q[i][0], font=font, fill=(0, 0, 0))
-            list_q[i][1] += 7 * len(list_q[i][0])
-            img = np.array(img_pil)
-    return img
-
-
 if __name__ == '__main__':
-    q = [[None, 0, 0] for i in range(50)]
-    # q = Array("b", [[None, 0, 0] for i in range(50)])
-    thread1 = Thread(target=rcv_comment)
-    thread1.setDaemon(True)
-    # thread2 = Thread(target=slide)
-    thread1.start()
-    # thread2.start()
-    while True:
-        img = slide(q)
-        cv2.namedWindow('screen', cv2.WINDOW_NORMAL)
-        cv2.setWindowProperty(
-            'screen', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        cv2.imshow("screen", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
+    th = Thread(target=rcv_comment)
+    th.start()
+    root.bind("<Key-n>", next)
+    root.bind("<Key-p>", prev)
+    root.mainloop()
